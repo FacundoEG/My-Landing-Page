de2a110c2a053c922eccc5e9589f83613390b5cd
Facundo EG <F_E_G.93@hotmail.com>
2021-07-06 01:05:45 -0300

    traje el welcome y presentation con contenfull

--- index/index.js
+++ index/index.js
@@ -14,8 +14,50 @@ function hambuguerMenuWindow() {
   });
 }
 
+function importWelcomeContent() {
+  fetch(
+    "https://cdn.contentful.com/spaces/qrsguk0kca31/environments/master/entries?access_token=dbzToas8F8Td4jFrmeLd843c69EgYp9q0JJVOSR2jvk&content_type=welcome"
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((r) => {
+      const titleWelcome = document.querySelector(".welcome-container__title");
+      const subtitleWelcome = document.querySelector(
+        ".welcome-container__subtitle"
+      );
+
+      for (const item of r.items) {
+        titleWelcome.textContent = item.fields.pagetitle;
+        subtitleWelcome.textContent = item.fields.pagesubtitle;
+      }
+    });
+}
+
+function importAboutMeContent() {
+  fetch(
+    "https://cdn.contentful.com/spaces/qrsguk0kca31/environments/master/entries?access_token=dbzToas8F8Td4jFrmeLd843c69EgYp9q0JJVOSR2jvk&content_type=presentation"
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((r) => {
+      const aboutMeTitle = document.querySelector(".about-me__title");
+      const aboutMeText = document.querySelector(".about-me__text");
+
+      for (const item of r.items) {
+        aboutMeTitle.textContent = item.fields.titulo;
+        aboutMeText.textContent = item.fields.texto;
+      }
+    });
+}
+
 function main() {
   hambuguerMenuWindow();
+  importWelcomeContent();
+  importAboutMeContent();
+
+  console.log("Soy el main");
 }
 
 main();
