26c92451df0bdb79d3a113a507d2ad98b5ae4427
Facundo EG <F_E_G.93@hotmail.com>
2021-07-08 17:12:37 -0300

    terminado el portfolio.html

--- comp/header/header.js
+++ comp/header/header.js
@@ -1,20 +1,20 @@
 function insertHeader(element) {
   const headerCompEl = document.createElement("header");
   headerCompEl.innerHTML = `     <header class="header-comp">
-  <a href="/index.html"><img src="https://raw.githubusercontent.com/FacundoEG/desafio-dwf-m4/main/images/logo.png" class="header-comp__logo"></a>
+  <a href="./index.html"><img src="https://raw.githubusercontent.com/FacundoEG/desafio-dwf-m4/main/images/logo.png" class="header-comp__logo"></a>
   <div class="header-comp__window-menu">
    <button class="header-comp__window-closebutton">
      <img src="https://raw.githubusercontent.com/FacundoEG/desafio-dwf-m4/main/images/x-mark.png">
    </button>
    <div class="header-comp__window-menu-link">
-     <a href="">Portfolio</a>
+     <a href="./portfolio.html">Portfolio</a>
      <a href="./servicios.html">Mis Servicios</a>
      <a href="">Contacto</a>
    </div>
   </div>
   <div class="header-comp__menu">
     <div class="header-comp__links-container">
-      <a href="">Portfolio</a>
+      <a href="./portfolio.html">Portfolio</a>
       <a href="./servicios.html">Mis Servicios</a>
       <a href="">Contacto</a>
     </div>
