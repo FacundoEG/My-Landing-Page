da405208e5c2e63b400729db12a3d08aa60a54b3
Facundo EG <F_E_G.93@hotmail.com>
2021-07-08 22:39:37 -0300

    desafío finalizado

--- contacto/contacto.js
+++ contacto/contacto.js
@@ -1,9 +1,12 @@
 function main() {
   const headerContainer = document.querySelector(".header-container");
   const footerContainer = document.querySelector(".footer-container");
+  const contactContainer = document.querySelector(".contacto-container");
   insertHeader(headerContainer);
   hambuguerMenuWindow();
   insertFooter(footerContainer);
+  insertContact(contactContainer);
+  sendFormData();
 }
 
 main();
--- index/index.js
+++ index/index.js
@@ -87,43 +87,18 @@ function importarServices() {
     });
 }
 
-function sendFormData() {
-  const myContactForm = document.querySelector(".contact-form");
-  myContactForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    const form = event.target;
-    const formData = new FormData(form);
-    const formDataFinal = {
-      Nombre: formData.get("nombre"),
-      Email: formData.get("email"),
-      Mensaje: formData.get("mensaje"),
-    };
-
-    const data = {
-      to: formDataFinal.Email,
-      message: formDataFinal.Mensaje,
-    };
-
-    fetch("https://apx-api.vercel.app/api/utils/dwf", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: { "content-type": "application/json" },
-    })
-      .then((res) => res.json())
-      .catch((error) => console.error("Ocurrio un error!", error))
-      .then((res) => console.log("Salio todo bien!", res));
-  });
-}
-
 function main() {
   const headerContainer = document.querySelector(".header-container");
   const footerContainer = document.querySelector(".footer-container");
+  const contactContainer = document.querySelector(".contact-container");
+
   importWelcomeContent();
   importAboutMeContent();
   importarServices();
   insertHeader(headerContainer);
   hambuguerMenuWindow();
   insertFooter(footerContainer);
+  insertContact(contactContainer);
   sendFormData();
 }
 
